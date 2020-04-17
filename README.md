27534870bb9f5df60e5e0b166c97ff0792387989
sastoudt <sstoudt@berkeley.edu>
2020-04-17 12:16:12 -0700

    add notes, add income color

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # BART_ride_viz
-visualize BART rides over time
+visualize BART rides over time, change due to COVID
+
+
+## Data
+
+- [Original Income Data, Census County Division](https://data.census.gov/cedsci/table?q=S1903&g=0400000US06.060000&tid=ACSST5Y2018.S1903&hidePreview=true&moe=false)
+- station income wrangling in stations.csv, stationIncome.R, incomeData.csv to produce stationIncomeNice.csv
+- [BART Usage Data](http://64.111.127.166/origin-destination/date-hour-soo-dest-2020.csv.gz)
+
+## How to Interpret Plots
+
+- Line width: thick to thin (more to less ridership)
+- Line Color: red to yellow (more to less ridership)
+- Point Color: tan to green (smaller to larger median household income)
+
+## For Context
+
+- [BART Station Abbreviations](https://api.bart.gov/docs/overview/abbrev.aspx)
+- [BART Map](https://en.wikipedia.org/wiki/List_of_Bay_Area_Rapid_Transit_stations#/media/File:BART_2019_official_map.svg) 
--- shiny/server.R
+++ shiny/server.R
@@ -3,26 +3,34 @@ function(input, output) {
   bart <- read_csv("../data/date-hour-soo-dest-2020.csv", col_names=F)
   names(bart)=c("date","hour","from", "to","count")
   
+  stationIncome = read.csv("../data/stationIncomeNice.csv", stringsAsFactors = F)
+  
   output$main_plot1 <- renderPlot({
-    names(bart)=c("date","hour","from", "to","count")
     bartT = bart %>% group_by(date, from, to) %>% summarise(total = sum(count))
     
     bartT$date=as.Date(as.character(bartT$date))
     
     test = subset(bartT,date==input$date1 & from ==input$station)
+    #test = subset(bartT, date=="2020-02-20" & from =="CAST")
     test$from = as.character(test$from)
     test$to=as.character(test$to)
     
+    test = merge(test, stationIncome, by.x="to", by.y="stationName", all.x=T, all.y=F)
   
     test$col <- heat.colors(10,rev = T)[as.numeric(cut(log(test$total),breaks = 10))]
     
-
+    test$col2 <- terrain.colors(10,rev = T)[as.numeric(cut(log(test$medianHouseholdIncome),breaks = 10))]
     
-    test = test %>% arrange(desc(total))
+    start = test[which(test$to==test$from),]
+    rest = test[-which(test$to==test$from),]
+    rest = rest %>% arrange(desc(total))
+    
+    test = rbind.data.frame(start, rest)
+
     test$orderId=1:nrow(test)
     
     stops <- data.frame(name=unique(c(test$from, test$to)))
-    relations <- test[,-1]
+    relations <- test[,-2] ## remove date
     g <- graph_from_data_frame(relations, directed=TRUE, vertices=stops)
     
     
@@ -30,10 +38,11 @@ function(input, output) {
     values = get.edge.attribute(g, "col")
     newOrder = get.edge.attribute(g, "orderId")
     total = get.edge.attribute(g,"total")
+    incomeColor = get.edge.attribute(g, "col2")
     
     
     arcplot(bart_edges,
-            col.arcs = values, ordering=newOrder,lwd.arcs=log(total), main=input$date1, col.labels="black")
+            col.arcs = values, ordering=newOrder,lwd.arcs=log(total), main=input$date1, col.labels="black", col.nodes = incomeColor)
     
     
     
@@ -50,16 +59,23 @@ function(input, output) {
     test$from = as.character(test$from)
     test$to=as.character(test$to)
     
+    test = merge(test, stationIncome, by.x="to", by.y="stationName", all.x=T, all.y=F)
+    
     
     test$col <- heat.colors(10,rev = T)[as.numeric(cut(log(test$total),breaks = 10))]
     
+    test$col2 <- terrain.colors(10,rev = T)[as.numeric(cut(log(test$medianHouseholdIncome),breaks = 10))]
     
+    start = test[which(test$to==test$from),]
+    rest = test[-which(test$to==test$from),]
+    rest = rest %>% arrange(desc(total))
     
-    test = test %>% arrange(desc(total))
+    test = rbind.data.frame(start, rest)
+    
     test$orderId=1:nrow(test)
     
     stops <- data.frame(name=unique(c(test$from, test$to)))
-    relations <- test[,-1]
+    relations <- test[,-2] ## remove date
     g <- graph_from_data_frame(relations, directed=TRUE, vertices=stops)
     
     
@@ -67,10 +83,11 @@ function(input, output) {
     values = get.edge.attribute(g, "col")
     newOrder = get.edge.attribute(g, "orderId")
     total = get.edge.attribute(g,"total")
+    incomeColor = get.edge.attribute(g, "col2")
     
     
     arcplot(bart_edges,
-            col.arcs = values, ordering=newOrder,lwd.arcs=log(total), main=input$date2, col.labels="black")
+            col.arcs = values, ordering=newOrder,lwd.arcs=log(total), main=input$date2, col.labels="black", col.nodes = incomeColor)
     
     
     
